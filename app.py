62105a14a08ec40d600649edfe3c95d53cd15794
tigernikhil <nikhilrawnka@gmail.com>
2019-04-10 11:41:13 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def find_bad_qn(a, url, file_name, fileName):
 	try:
 		# pdb.set_trace()
 		# redis_host = os.getenv('REDIS_URL', 'localhost')
-		conn = redis.StrictRedis(REDIS_HOST, charset="utf-8", decode_responses=True)
+		conn = redis.StrictRedis.from_url(REDIS_HOST, charset="utf-8", decode_responses=True)
 		# context = ssl._create_unverified_context()
 		# resp = urllib.request.urlopen(url, context=context)
 		context = ssl._create_unverified_context()
@@ -178,7 +178,7 @@ class StringGeneratorWebService(object):
 
 	@cherrypy.tools.accept(media='text/plain')
 	def GET(self):
-		conn = redis.StrictRedis(REDIS_HOST, charset="utf-8", decode_responses=True)
+		conn = redis.StrictRedis.from_url(REDIS_HOST, charset="utf-8", decode_responses=True)
 		# namevaluex = conn.hgetall('500002')
 		# namevaluextest = conn.hgetall()
 		# print(namevaluex['SC_CODE'])
@@ -215,7 +215,7 @@ class StringGeneratorWebService(object):
 	def POST(self, another_string):
 		# pdb.set_trace()
 		dataShow = []
-		conn = redis.StrictRedis(host=REDIS_HOST, decode_responses=True)
+		conn = redis.StrictRedis.from_url(host=REDIS_HOST, decode_responses=True)
 		val = "*" + another_string.upper() + "*"
 		count = 0
 		for user in conn.keys(val):
